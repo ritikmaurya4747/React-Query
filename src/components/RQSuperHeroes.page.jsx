86398082362e4d8abb8d40164c9a6f6a4e120b5e
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-22 18:42:12 +0530

    adding select funtion to get data with  using data transformation

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -4,24 +4,28 @@ import { useQuery } from "react-query";
 
 function RQSuperHeroesPage() {
   const fetchSuperHeros = () => {
-    return axios.get("http://localhost:4000/superheroesa");
+    return axios.get("http://localhost:4000/superheroes");
   };
 
   const onSuccess = (data) => {
-    console.log("Perform side effect after data fetching",data);
-}
+    console.log("Perform side effect after data fetching", data);
+  };
   const onError = (error) => {
-    console.log("Perform side effect after encountering error",error);
-}
+    console.log("Perform side effect after encountering error", error);
+  };
 
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroe",
     fetchSuperHeros,
-    { 
-        enabled: false,
-        onSuccess,
-        onError,
-     }
+    {
+      enabled: false,
+      onSuccess,
+      onError,
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+        return superHeroNames;
+      },
+    }
   );
 
   console.log({ isLoading, isFetching });
@@ -34,7 +38,8 @@ function RQSuperHeroesPage() {
   }
   return (
     <>
-      <div className="">
+      {/* // ye wala code use karne ke liye uper select wala funtion remove or comment karne ke baad hi use kar sakate hain */}
+      {/* <div className="">
         <h2 className="text-center mt-10 py-3 text-3xl font-bold">
           RQ Super Heroes Page
         </h2>
@@ -53,6 +58,34 @@ function RQSuperHeroesPage() {
               {hero.name}
             </p>
           ))}
+        </div>
+      </div>  */}
+
+      {/* ------------------------------------------------------------------- */}
+
+      {/* ye wala select wale funtion ke liye code hain to fetch data same as up  */}
+      <div>
+        <h2 className="text-center mt-10 py-3 text-3xl font-bold">
+          RQ Super Heroes Page
+        </h2>
+        <div className="flex flex-col items-center my-6">
+          <button
+            className="px-3 py-2 text-white font-bold mb-5 bg-blue-600 rounded-md flex justify-center items-center"
+            onClick={refetch}
+          >
+            Fetch heroes
+          </button>
+          {data &&
+            data.map((heroName) => {
+              return (
+                <div
+                  key={heroName}
+                  className="text-center text-red-500 text-2xl font-bold"
+                >
+                  {heroName}
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
