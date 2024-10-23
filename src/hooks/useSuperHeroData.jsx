d7cdbef01b0dc559ba0e22cda175cb52b0172403
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-23 17:17:47 +0530

    Intial query data

--- src/hooks/useSuperHeroData.jsx
+++ src/hooks/useSuperHeroData.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 // const fetchSuperHero = (heroId) => {
 //   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
@@ -14,5 +14,17 @@ const fetchSuperHero = ({queryKey}) => {
     return axios.get(`http://localhost:4000/superheroes/${heroId}`);
   };
   export const useSuperHeroData = (heroId) => {
-    return useQuery(["super-hero", heroId],  fetchSuperHero);
+    const queryClient = useQueryClient()
+    return useQuery(["super-hero", heroId],  fetchSuperHero,{
+      initialData: () =>{
+        const hero = queryClient.getQueryData('super-hero')?.data?.find(hero => hero.id ===parseInt(heroId))
+        if(hero){
+          return {
+            data: hero
+          }
+        } else{
+          return undefined
+        }
+      }
+    });
   };
